fd36980d914f8930ce0831ec10243b41b22af69d
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2025-01-02 14:37:24 +0600

    banner and category set up

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -1,7 +1,17 @@
 const Navbar = () => {
+  const navLinks = (
+    <>
+      <li>
+        <a>Item 1</a>
+      </li>
+      <li>
+        <a>Item 3</a>
+      </li>
+    </>
+  );
   return (
     <>
-      <div className="navbar bg-base-100">
+      <div className="navbar fixed z-50 bg-black bg-opacity-30 text-white max-w-screen-xl">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -23,49 +33,13 @@ const Navbar = () => {
             <ul
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
+            >{navLinks}</ul>
           </div>
           <a className="btn btn-ghost text-xl">daisyUI</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+          {navLinks}
           </ul>
         </div>
         <div className="navbar-end">
---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,45 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+
+import slide1 from "../../../assets/home/slide1.jpg";
+import slide2 from "../../../assets/home/slide2.jpg";
+import slide3 from "../../../assets/home/slide3.jpg";
+import slide4 from "../../../assets/home/slide4.jpg";
+const Category = () => {
+  return (
+    <>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper mb-10"
+      >
+        <SwiperSlide>
+          <img className="w-full" src={slide1} alt="" />
+          <p className="text-4xl text-white -mt-20 mb-5 text-center">Salads</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="w-full" src={slide2} alt="" />
+          <p className="text-4xl text-white -mt-20 mb-5 text-center">Pizzas</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="w-full" src={slide3} alt="" />
+          <p className="text-4xl text-white -mt-20 mb-5 text-center">Soups</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="w-full" src={slide4} alt="" />
+          <p className="text-4xl text-white -mt-20 mb-5 text-center">Dessert</p>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+
+export default Category;
