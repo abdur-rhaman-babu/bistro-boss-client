7b51a22c73e7726dc26bea41b9c55715c6301c83
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2025-01-02 15:21:43 +0600

    create dynamic title

---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,10 @@
+const SectionTitle = ({heading, subHeading}) => {
+    return (
+        <div className="md:w-4/12 text-center mx-auto">
+            <p className="font-semibold text-yellow-500">---- {subHeading} ----</p>
+            <h1 className="font-semibold text-3xl border-y-2 p-4 mb-5 uppercase">{heading}</h1>
+        </div>
+    );
+};
+
+export default SectionTitle;
--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >{navLinks}</ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">Bistro Boss</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
